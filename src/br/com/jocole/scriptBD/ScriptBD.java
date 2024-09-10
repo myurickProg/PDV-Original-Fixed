77bf8ad2d01dc6e5c048f464fa60aae2ca59c46c
Erick Silva <biladero528@gmail.com>
2024-09-10 10:40:43 -0300

    primeiro commit

---
+++ src/br/com/jocole/scriptBD/ScriptBD.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.jocole.scriptBD;
+
+/**
+ *
+ * @author yuriv
+ */
+public class ScriptBD {
+    //Codigo para acessar a aplicacao
+    //CREATE TABLE dbjocole;
+    //USE dbjocole;
+    //CREATE TABLE tbusers (iduser INTEGER AUTO_INCREMENT PRIMARY KEY, Login VARCHAR(30) NOT NULL, Senha VARCHAR(30) NOT NULL, Nome VARCHAR(20) NOT NULL);
+    //INSERT INTO tbusers(Login, Senha, Nome)VALUES ('admin', 'admin', 'Administrado');
+    
+}
